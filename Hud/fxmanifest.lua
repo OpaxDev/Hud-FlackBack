af44402d3192543f5d646ea22fec3855abea5e31
Opax.Dev <129586172+OpaxDev@users.noreply.github.com>
2023-04-02 15:34:25 +0200

    Add files via upload

---
+++ Hud/fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'cerulean'
+games {'gta5'}
+
+author 'LTZ KORP'
+description 'HUD Flashland'
+version '1.0.0'
+
+client_scripts {
+    'client.lua',
+}
+
+ui_page 'html/ui.html'
+
+files {
+	'html/ui.html',
+	'html/style.css',
+	'html/grid.css',
+	'html/main.js',
+	'html/img/hunger.png',
+    'html/img/hunger3.png',
+	'html/img/water.png',
+    'html/img/water3.png',
+	'html/img/speaker1.png',
+	'html/img/speaker2.png',
+	'html/img/speaker3.png'
+
+}
